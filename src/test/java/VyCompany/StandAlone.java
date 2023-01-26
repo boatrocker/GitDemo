e9180eb32221a59536c50b31c7fe78fea972fb50
vaibh <vaibhavyelwantge1997@gmail.com>
2023-01-26 23:36:58 +0530

    first commit

---
+++ src/test/java/VyCompany/StandAlone.java
@@ -0,0 +1,18 @@
+package VyCompany;
+
+
+public class StandAlone {
+
+
+
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		WebDriverManager.chromedriver().setup
+		
+
+	}
+
+}
